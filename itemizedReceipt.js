d7a3e3913baf25fab2282022d8fc9ddad8176f98
Jeffrey Dunaway <jeffreydunaway@users.noreply.github.com>
2022-11-08 16:50:22 -0800

    itemizedReceipt_Week3_HW
    Week 3 HW

--- itemizedReceipt.js
+++ itemizedReceipt.js
@@ -1,9 +1,15 @@
-// create function logReceipt that accepts menu items (1 or many) as objects
-// with these properties: {descr, price}
-// i.e. {descr: 'Coke', price: 1.99}
-// function should log each item to the console and log a total price
-
-
+const priceTotal =() => {
+}
+const logReceipt = (...jeffsMenu) => { 
+  let subTotal = 0;
+  jeffsMenu.forEach(jeffsMenu => {
+    console.log(`${jeffsMenu.descr} - ${jeffsMenu.price}`)
+    subTotal += jeffsMenu.price;
+  })
+  let taxes = subTotal * 0.10;
+  let total = taxes + subTotal;
+  console.log(`Subtotal = ${subTotal}\nTaxes = ${taxes.toFixed(2)}\nTotal = ${total.toFixed(2)}`)
+};
 
 // Check
 logReceipt(
@@ -11,8 +17,64 @@ logReceipt(
   { descr: 'Chips & Salsa', price: 2.99 },
   { descr: 'Sprite', price: 1.99 }
 );
+
+
 // should log something like:
 // Burrito - $5.99
 // Chips & Salsa - $2.99
 // Sprite - $1.99
-// Total - $10.97
+// SubTotal - $10.97
+// Tax = $1.10
+// Total = $12.07
+
+// A(n) 'this' allows you to call the function above where the function is defined.
+
+// //
+// const priceTotal =() => {
+//   }
+// const logReceipt =(...jeffsMenu) => {
+// let subTotal = 0; 
+// jeffsMenu.forEach(jeffsMenu => {
+//   console.log(`${jeffsMenu.descr} - ${jeffsMenu.price}`)
+//   subTotal += jeffsMenu.price;
+// })
+// let taxes = subTotal * 0.20; 
+// let total = taxes + subTotal; 
+// console.log(`Subtotal = ${subTotal}\nTaxes = ${taxes.toFixed(2)}\nTotal = ${total.toFixed(2)}`)
+// };
+// const withTotal = function (price) {
+//   totalPrice += price
+// }
+// // jeffsMenu.forEach((jeffsMenu)=>{
+// //   console.log(jeffsMenu.descr)
+// //   withTotal(jeffsMenu.price)
+// //   totalPrice += jeffsMenu.price
+// //   console.log(jeffsMenu.price)
+// // })
+// //console.log(totalPrice/2)}
+
+// // Check
+// // const logReceipt = function (...jeffsMenu) {
+// //   let totalTaxPrice = 0; 
+  
+// //   const withTotal = function (price) {
+// //     totalTaxPrice += price
+// //   }
+// //   jeffsMenu.forEach((jeffsMenu)=>{
+// //     console.log(jeffsMenu.descr)
+// //     withTotal(jeffsMenu.price)
+// //     totalTaxPrice += jeffsMenu.price
+// //     console.log(jeffsMenu.price)
+// //   })
+  
+// //   console.log(totalTaxPrice/2 * .09)
+// //   const TaxTotal = totalTaxPrice + TotalPrice 
+// //   console.log(TaxTotal)
+// //   }
+  
+// //   // Check
+// //   logReceipt(
+// //     { descr: 'Burrito', price: 5.99 },
+// //     { descr: 'Chips & Salsa', price: 2.99 },
+// //     { descr: 'Sprite', price: 1.99 }
+// //   );
