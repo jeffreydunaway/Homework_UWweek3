c18709d7036aac1095d89ab2e7c8927e6e69a417
Jeffrey Dunaway <jeffreydunaway@users.noreply.github.com>
2022-11-08 16:52:26 -0800

    spaceShip_Week3
    Week 3 spaceShip HW

--- spaceShip.js
+++ spaceShip.js
@@ -3,7 +3,22 @@
 // - should have a method accelerate that logs to the console 
 //   `${name} moving to ${topSpeed}`
 
+class SpaceShip {
+    constructor(name, topSpeed){
+        this.name= name; 
+        this.topSpeed = topSpeed; 
+    }
+    accelerate() { 
+        console.log(`${this.name} uses ${this.topSpeed}`);
+    }
+}
+
+const bubbie = new SpaceShip('Bubbie', 1000)
+const stinks = new SpaceShip('Stinks', 'Engaged at Warp 9.9')
 
 
 // 2. Call the constructor with a couple ships, 
 // and call accelerate on both of them.
+
+stinks.accelerate();
+bubbie.accelerate();
